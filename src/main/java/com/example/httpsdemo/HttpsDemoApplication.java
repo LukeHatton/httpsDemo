e141d815d3096295cbd8a0d3717590926ce220f7
lizhao <lukehatton685@gmail.com>
2023-03-22 21:13:17 +0800

    添加自定义配置属性
    避免硬编码机要信息

--- src/main/java/com/example/httpsdemo/HttpsDemoApplication.java
+++ src/main/java/com/example/httpsdemo/HttpsDemoApplication.java
@@ -2,8 +2,10 @@ package com.example.httpsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class HttpsDemoApplication {
 
   public static void main(String[] args) {
